1ebe0987c766ff1069bc3315a394085033dbe1d9
yilber822 <yilber822@gmail.com>
2018-11-07 16:49:35 -0500

    Ya es la prueba final

--- src/hola.java
+++ src/hola.java
@@ -3,7 +3,7 @@ public class hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub xDDD
-		System.out.println("Hola Mundo/Huercas");
+		System.out.println("Hola Mundo/Huercas/Trolazos");
 
 	}
 
